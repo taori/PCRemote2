5cb1b08fc322ac9814fc34fd1b95ca3bbb2ddbf1
Andreas Müller <taori@users.noreply.github.com>
2021-08-08 15:38:26 +0200

    initial wix preparations

---
+++ deploy/Amusoft.Toolkit.Wix/ConsoleApplication.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Amusoft.Toolkit.Wix.Features;
+using CommandDotNet;
+
+namespace Amusoft.Toolkit.Wix
+{
+	public class ConsoleApplication
+	{
+		public class Generate
+		{
+			[Command(
+				Description = "Generates *.wxs file based on the files within a directory", 
+				Name = "productWxsFromDirectory")]
+			public async Task<int> ProductWxsFromDirectory(string directory, string targetWxsPath, string ignoreFile)
+			{
+				var feature = new GenerateProductWxsFromDirectoryFeature(directory, targetWxsPath, ignoreFile);
+				return await feature.ExecuteAsync();
+			}
+		}
+	}
+}
---
+++ deploy/Amusoft.Toolkit.Wix/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using CommandDotNet;
+
+namespace Amusoft.Toolkit.Wix
+{
+	class Program
+	{
+		static async Task<int> Main(string[] args)
+		{
+			var appRunner = new AppRunner<ConsoleApplication>();
+
+			return await appRunner
+				.UseVersionMiddleware()
+				.RunAsync();
+		}
+	}
+}
